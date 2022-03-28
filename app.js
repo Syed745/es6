68340890571fe5561a7107bdaade3d4daaf6e88d
Syed745 <syedb1997@gmail.com>
2022-03-28 23:12:20 +0500

    done

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+// let a = [1,2,3,4,5,6];
+
+let b = 
+  {
+    id: 1,
+    name: "Muhammad Ali",
+    category: "e",
+  }
+
+  let keys = Object.keys(b);
+  let valu = Object.values(b)
+  let ent = Object.entries(b)
+  console.log(keys,valu)
+  console.log(ent)
+
+
+
+
